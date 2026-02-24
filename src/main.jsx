f1d03b75622fca95bea94f6c8c7b505ba192425d
Sandhiyalaxmii <sandhiyamuniappan955@gmail.com>
2026-02-24 12:50:49 +0530

    portfolio built

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { ThemeProvider } from "./context/ThemeContext";
+import { ThemeProvider } from "./Context/ThemeContext";
 import App from "./App";
 import "./index.css";
 
